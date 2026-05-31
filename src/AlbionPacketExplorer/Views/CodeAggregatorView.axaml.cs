08be62da775def0f1b6c44959e78841cdf958e9e
Constantin <bimasakti.ro@gmail.com>
2026-06-01 01:08:19 +0200

    ui(viewmodels,views): add filter preset row, auto-select newest, column width persistence
    PacketListView: preset name/load/delete toolbar row, Auto-select Newest context menu toggle.
    CodeAggregatorView: column width persistence via ColumnWidthHelper.
    Auto-select newest fires ScrollToRowRequested event; view subscribes via DataContextChanged.

--- src/AlbionPacketExplorer/Views/CodeAggregatorView.axaml.cs
+++ src/AlbionPacketExplorer/Views/CodeAggregatorView.axaml.cs
@@ -4,9 +4,13 @@ namespace AlbionPacketExplorer.Views;
 
 public partial class CodeAggregatorView : UserControl
 {
+    public DataGrid Grid => MainGrid;
+
     public CodeAggregatorView()
     {
         InitializeComponent();
+        Loaded   += (_, _) => ColumnWidthHelper.Restore(MainGrid, "aggregator");
+        Unloaded += (_, _) => ColumnWidthHelper.Save(MainGrid, "aggregator");
     }
 
     protected override void OnAttachedToVisualTree(Avalonia.VisualTreeAttachmentEventArgs e)
--- src/AlbionPacketExplorer/Views/PacketListView.axaml.cs
+++ src/AlbionPacketExplorer/Views/PacketListView.axaml.cs
@@ -1,11 +1,34 @@
 using Avalonia.Controls;
+using Avalonia.Threading;
+using AlbionPacketExplorer.ViewModels;
 
 namespace AlbionPacketExplorer.Views;
 
 public partial class PacketListView : UserControl
 {
+    public DataGrid Grid => MainGrid;
+
+    private PacketListViewModel? _vm;
+
     public PacketListView()
     {
         InitializeComponent();
+        DataContextChanged += (_, _) =>
+        {
+            if (_vm != null) _vm.ScrollToRowRequested -= OnScrollToRowRequested;
+            _vm = DataContext as PacketListViewModel;
+            if (_vm != null) _vm.ScrollToRowRequested += OnScrollToRowRequested;
+        };
+        Loaded   += (_, _) => ColumnWidthHelper.Restore(MainGrid, "packetlist");
+        Unloaded += (_, _) => ColumnWidthHelper.Save(MainGrid, "packetlist");
+    }
+
+    private void OnScrollToRowRequested(PacketRow row)
+    {
+        Dispatcher.UIThread.Post(() =>
+        {
+            MainGrid.SelectedItem = row;
+            MainGrid.ScrollIntoView(row, null);
+        }, DispatcherPriority.Background);
     }
 }
